c0fe615a98a1838bf2a1c7d4a9996fe7b890d22a
DGator86 <djvogeli@gmail.com>
2026-03-08 15:37:13 -0400

    Add information theory indicators and integrate into UI/registry

--- phi/indicators/__init__.py
+++ phi/indicators/__init__.py
@@ -11,6 +11,12 @@ from phi.logging import get_logger
 logger = get_logger(__name__)
 
 from .orderflow import OHLCVOrderFlowProvider, get_order_flow_provider, set_order_flow_provider
+from .information import (
+    compute_entropy_signal,
+    compute_fisher_information_signal,
+    compute_kld_signal,
+    compute_mutual_info_signal,
+)
 from .simple import compute_bollinger, compute_breakout, compute_dual_sma, compute_macd, compute_mean_reversion, compute_rsi
 from .simple import INDICATOR_COMPUTERS
 
@@ -25,4 +31,8 @@ __all__ = [
     "OHLCVOrderFlowProvider",
     "set_order_flow_provider",
     "get_order_flow_provider",
+    "compute_entropy_signal",
+    "compute_mutual_info_signal",
+    "compute_fisher_information_signal",
+    "compute_kld_signal",
 ]
